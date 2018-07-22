eca3c3d1553cfa419bba8f880abdc15937105d3a
syrkles <syrkles@gmail.com>
2018-07-22 14:07:18 -0400

    Update liquid.mk

--- liquid.mk
+++ liquid.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/whyred/device.mk)
 
 # Inherit some common Liquid stuff.
-$(call inherit-product, vendor/liquid/config/full_common_phone.mk)
+$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 TARGET_VENDOR := Xiaomi
