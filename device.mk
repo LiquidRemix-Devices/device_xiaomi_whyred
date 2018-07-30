b6a13f2cceef542676d3a357addae0d8ef7bc3e1
syrkles <syrkles@gmail.com>
2018-07-30 03:33:35 +0000

    Revert "whyred: Add XiaomiParts"
    This reverts commit fdf2cee27496ba1be21dfc231218f84cdddc7f4e.

--- device.mk
+++ device.mk
@@ -460,8 +460,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
-
-# XiaomiParts
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
